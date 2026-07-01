5705132e1dd57f9133a3636458ae49fc8929b968
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2026-07-01 22:31:05 +0100

    fix: use new JSX transformation for react

--- src/generators/bundle.ts
+++ src/generators/bundle.ts
@@ -1,7 +1,13 @@
+import path from 'node:path';
+
 import type {ProjectModel} from '../model/index.js';
 import {addDevDep, addScript} from '../utils/pm.js';
+import type {FileSystem} from '../utils/types.js';
 
-export function generateBundling(model: ProjectModel) {
+import renderAppBuilderConfigJs from './templates/app-builder.config.js.hbs.js';
+import renderAppBuilderConfigTs from './templates/app-builder.config.ts.hbs.js';
+
+export function generateBundling(model: ProjectModel, fs: FileSystem) {
     const hasAppBuilder = model.hasBackend || model.hasFrontend;
 
     if (hasAppBuilder) {
@@ -9,6 +15,20 @@ export function generateBundling(model: ProjectModel) {
         addScript(model, 'start', 'node dist/server/index.js');
         addScript(model, 'dev', 'app-builder dev');
         addScript(model, 'build', 'NODE_ENV=production app-builder build');
+
+        if (model.hasReact) {
+            if (model.language === 'ts') {
+                fs.writeFile(
+                    path.join(model.destination, 'app-builder.config.ts'),
+                    renderAppBuilderConfigTs({}),
+                );
+            } else {
+                fs.writeFile(
+                    path.join(model.destination, 'app-builder.config.js'),
+                    renderAppBuilderConfigJs({}),
+                );
+            }
+        }
     } else if (model.language === 'ts') {
         addScript(model, 'start', 'node dist/index.js');
         addScript(model, 'dev', 'tsc --watch');
