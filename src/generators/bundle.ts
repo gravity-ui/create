82ed425a50c89ca9031c7f22a033e254482ac801
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2026-07-01 17:21:10 +0100

    fix: bundling

--- src/generators/bundle.ts
+++ src/generators/bundle.ts
@@ -1,14 +1,15 @@
 import type {ProjectModel} from '../model/index.js';
-import {addScript} from '../utils/pm.js';
+import {addDevDep, addScript} from '../utils/pm.js';
 
 export function generateBundling(model: ProjectModel) {
     const hasAppBuilder = model.hasBackend || model.hasFrontend;
 
     if (hasAppBuilder) {
+        addDevDep(model, '@gravity-ui/app-builder', '^0.48.0');
         addScript(model, 'start', 'node dist/server/index.js');
         addScript(model, 'dev', 'app-builder dev');
         addScript(model, 'build', 'NODE_ENV=production app-builder build');
-    } else {
+    } else if (model.language === 'ts') {
         addScript(model, 'start', 'node dist/index.js');
         addScript(model, 'dev', 'tsc --watch');
         addScript(model, 'build', 'tsc -p tsconfg.build.json');
